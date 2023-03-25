ebc191c2a0517e6d0a59e0942193c3cabe2a60e5
Emily Zhou <zhouxemily@gmail.com>
2023-03-25 08:30:25 -0700

    Add all POPANE studies
    Signed-off-by: Emily Zhou <zhouxemily@gmail.com>

--- src/tools/data_reader_popane.py
+++ src/tools/data_reader_popane.py
@@ -12,6 +12,13 @@ class Study1:
     ALL = [BASELINE, POSITIVE_EMOTION_HIGH_APPROACH, THREAT, NEUTRAL]
 
 
+class Study2:
+    BASELINE = "Baseline"
+    POSITIVE_EMOTION_LOW_APPROACH = "Positive_Emotion_Low_Approach"
+    ANGER = "Anger4"
+    ALL = [BASELINE, POSITIVE_EMOTION_LOW_APPROACH, ANGER]
+
+
 class Study3:
     BASELINE = "Baseline"
     GRATITUDE = "Gratitude"
@@ -37,6 +44,45 @@ class Study5:
     AMUSEMENT3 = "Amusement3"
     AMUSEMENT4 = "Amusement4"
     ALL = [ANGER1, ANGER2, ANGER3, NEUTRAL1, NEUTRAL6, NEUTRAL7, AMUSEMENT2, AMUSEMENT3, AMUSEMENT4]
+
+
+class Study6:
+    BASELINE = "Baseline"
+    AMUSEMENT1 = "Amusement1"
+    AMUSEMENT2 = "Amusement2"
+    AMUSEMENT3 = "Amusement3"
+    AMUSEMENT4 = "Amusement4"
+    TENDERNESS1 = "Tenderness1"
+    TENDERNESS2 = "Tenderness2"
+    NEUTRAL1 = "Neutral1"
+    NEUTRAL2 = "Neutral2"
+    NEUTRAL3 = "Neutral3"
+    NEUTRAL4 = "Neutral4"
+    NEUTRAL5 = "Neutral5"
+    NEUTRAL6 = "Neutral6"
+    ANGER1 = "Anger1"
+    ANGER2 = "Anger2"
+    ANGER3 = "Anger3"
+    DISGUST = "Disgust"
+    FEAR = "Fear1"
+    SADNESS = "Sadness1"
+    ALL = [
+        BASELINE, AMUSEMENT1, AMUSEMENT2, AMUSEMENT3, AMUSEMENT4, 
+        TENDERNESS1, TENDERNESS2,
+        NEUTRAL1, NEUTRAL2, NEUTRAL3, NEUTRAL4, NEUTRAL5, NEUTRAL6,
+        ANGER1, ANGER2, ANGER3,
+        DISGUST, FEAR, SADNESS
+    ]
+
+
+class Study7:
+    BASELINE = "Baseline"
+    AMUSEMENT = "Amusement4"
+    ANGER = "Anger3"
+    EXCITEMENT = "Excitement"
+    NEUTRAL = "Neutral6"
+    SADNESS = "Sadness2"
+    ALL = [BASELINE, AMUSEMENT, ANGER, EXCITEMENT, NEUTRAL, SADNESS]
 
 
 HIGH_AROUSAL = [
@@ -74,9 +120,12 @@ class Paths:
     POPANE = os.path.join(DATA_DIR, "POPANE")
     METRICS = os.path.join(DATA_DIR, "metrics", "POPANE")
     STUDY1 = os.path.join(POPANE, "Study1")
+    STUDY2 = os.path.join(POPANE, "Study2")
     STUDY3 = os.path.join(POPANE, "Study3")
     STUDY4 = os.path.join(POPANE, "Study4")
     STUDY5 = os.path.join(POPANE, "Study5")
+    STUDY6 = os.path.join(POPANE, "Study6")
+    STUDY7 = os.path.join(POPANE, "Study7")
 
 
 def get_subjects(study_num):
